ec620a928d4bff38b4c782b5f1aa64e5fbdd82cf
Luca Rizzello <rizzello@iccvlabsrv22.iccluster.epfl.ch>
2017-05-05 14:41:37 +0200

    dataset and model names set by user

--- server.py
+++ server.py
@@ -14,6 +14,7 @@ import csv
 import bcrypt
 import subprocess
 import sys
+
 if sys.version_info[0] < 3:
     from cStringIO import StringIO
 else:
@@ -34,6 +35,7 @@ custom_base64chars = ")"
 def getImmediateSubdirectories(path):
     return [name for name in os.listdir(path)
             if os.path.isdir(os.path.join(path, name))]
+
 
 def checkUser(request):
     try:
@@ -62,22 +64,22 @@ def checkUser(request):
 @app.route('/api/login', methods=['GET'])
 def loginFun():
     if checkUser(request):
-	username = request.headers['username']
-	modelsPath = os.getcwd()+"/ccboost-service/workspace/"+username+"/models" #Note : For now there is only service, future work might add more services than just ccboost
-										  #for example the user would choose the service he wants to use before logging in
+        username = request.headers['username']
+        modelsPath = os.getcwd() + "/ccboost-service/workspace/" + username + "/models"  # Note : For now there is only service, future work might add more services than just ccboost
+        # for example the user would choose the service he wants to use before logging in
         if not os.path.isdir(modelsPath):
-		os.makedirs(modelsPath)
-	modelList = getImmediateSubdirectories(modelsPath)
-	dataPath = os.getcwd()+"/userInput/"+username
+            os.makedirs(modelsPath)
+        modelList = getImmediateSubdirectories(modelsPath)
+        dataPath = os.getcwd() + "/userInput/" + username
         if not os.path.isdir(dataPath):
-		os.makedirs(dataPath)
-	dataList = getImmediateSubdirectories(dataPath)
-	
-	#return np array of format [[dataset1,dataset2...],[modelA, modelB...]]
-	#Ilastik slots are np arrays and returning this in that format makes things simpler
-	#toReturn = np.array([dataList, modelsList])
-	f = StringIO()
-        np.savez(f, data = dataList, models = modelList)
+            os.makedirs(dataPath)
+        dataList = getImmediateSubdirectories(dataPath)
+
+        # return np array of format [[dataset1,dataset2...],[modelA, modelB...]]
+        # Ilastik slots are np arrays and returning this in that format makes things simpler
+        # toReturn = np.array([dataList, modelsList])
+        f = StringIO()
+        np.savez(f, data=dataList, models=modelList)
         f.seek(0)
         payload = f.read()
         return payload, 200
@@ -119,10 +121,11 @@ def sendPictureFun():
         labels = np.load(StringIO(bodyClear))['labels']
         image = np.load(StringIO(bodyClear))['image']
         username = request.headers['username']
-        tag = datetime.now().strftime("%Y%m%d%H%M%S")
+        datasetName = request.headers['datasetName']
+	modelName = request.headers['modelName']
 
         # save labels and data in h5 format
-        inputDirectory = os.getcwd() + '/userInput/' + username + "/" + tag + "/"
+        inputDirectory = os.getcwd() + '/userInput/' + username + "/" + datasetName + "/"
         if not os.path.isdir(inputDirectory):
             os.makedirs(inputDirectory)
 
@@ -155,39 +158,36 @@ def sendPictureFun():
         dir_path = os.getcwd() + "/ccboost-service/workspace"
 
         # write cfg file based on request
-        if not os.path.isdir(dir_path + "/"+username + "/config"):
-            os.makedirs(dir_path + "/"+username + "/config")
+        if not os.path.isdir(dir_path + "/" + username + "/config"):
+            os.makedirs(dir_path + "/" + username + "/config")
 
-        model_name = "ccboost2000stumps"
-        file = open(dir_path + "/"+username + "/config/" + tag + ".cfg", "w")
-        file.write("dataset_name = \'" + tag + "\'\n")
+        file = open(dir_path + "/" + username + "/config/" + datasetName + ".cfg", "w")
+        file.write("dataset_name = \'" + datasetName + "\'\n")
         file.write("root = \'/cvlabdata1/home/rizzello/ccboost-service\'\n")
-        file.write("stack = \'/cvlabdata1/home/rizzello/userInput/" + username + "/" + tag + "/data.h5\'\n") 
-        file.write("labels = \'/cvlabdata1/home/rizzello/userInput/" + username + "/" + tag + "/labels.h5\'\n") 
-        file.write("model_name = " + model_name + "\n")
+        file.write("stack = \'/cvlabdata1/home/rizzello/userInput/" + username + "/" + datasetName + "/data.h5\'\n")
+        file.write("labels = \'/cvlabdata1/home/rizzello/userInput/" + username + "/" + datasetName + "/labels.h5\'\n")
+        file.write("model_name = " + modelName + "\n")
         file.write("num_adaboost_stumps = 2000\n")
         file.close()
 
-	logPath = os.getcwd()+"/userLogs/"+username
-	if not os.path.isdir(logPath):
+        logPath = os.getcwd() + "/userLogs/" + username
+        if not os.path.isdir(logPath):
             os.makedirs(logPath)
-	logPath = logPath + "log.txt"
-	p = subprocess.Popen(
+        logPath = logPath + "log.txt"
+        p = subprocess.Popen(
             ["python",
              "ccboost-service/handler.py",
              "--train",
-             dir_path + "/"+username + "/config/" + tag + ".cfg",
+             dir_path + "/" + username + "/config/" + datasetName + ".cfg",
              "--username",
-             username,
-             "--tag",
-             tag], stdout=subprocess.PIPE, bufsize=1)
-	for line in iter(p.stdout.readline, b''):
-		print line,
-    		#file = open(logPath, "w")
-		#file.write(line)
-		#file.close(),
-	p.stdout.close()
-	p.wait()
+             username], stdout=subprocess.PIPE, bufsize=1)
+        for line in iter(p.stdout.readline, b''):
+            print line,
+        # file = open(logPath, "w")
+        # file.write(line)
+        # file.close(),
+        p.stdout.close()
+        p.wait()
 
         # fetch result and send it back
         h5 = h5py.File(
@@ -195,10 +195,9 @@ def sendPictureFun():
             "/" +
             username +
             "/runs/" +
-            tag +
-	    "/" +
-	    "/results/"+
-            model_name +
+            datasetName +
+            "/results/" +
+            modelName +
             "/out-0-ab-max.h5",
             driver=None)
         data = h5['data']
