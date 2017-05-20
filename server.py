fe0faa9ed538a13b3e974818613c9267c7a4991f
Luca Rizzello <rizzello@iccvlabsrv22.iccluster.epfl.ch>
2017-05-20 16:33:36 +0200

    added test support

--- server.py
+++ server.py
@@ -86,13 +86,35 @@ def loginFun():
     else:
         return '', 401
 
+@app.route('/api/downloadDataset', methods=['GET'])
+def downloadFun():
+	if checkUser(request):
+		dir_path = os.path.dirname(os.path.realpath(__file__))
+		username = request.headers['username']
+		datasetName = request.headers['dataset_name']
+		h5 = h5py.File(	dir_path +
+				"/userInput/"+
+				username+
+				"/"+
+				datasetName+
+				"/data.h5",
+				driver=None)
+        	data = h5['data']
+        	f = StringIO()
+        	np.savez_compressed(f, data=data)
+        	f.seek(0)
+        	compressed_data = f.read()
+        	return compressed_data, 200
+	else:
+		return '', 401
+
 
 @app.route('/api/getProgress', methods=['GET'])
 def progressFun():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     try:
-        request.headers['username']
-        file = open(dir_path + "/userLogs/rizzello/log.txt")
+        username = request.headers['username']
+        file = open(dir_path + "/userLogs/"+username+"/log.txt")
         txt = file.read()
         file.close()
         return txt, 200
@@ -100,8 +122,8 @@ def progressFun():
         return "", 404
 
 
-@app.route('/api/sendPicture', methods=['POST'])
-def sendPictureFun():
+@app.route('/api/train', methods=['POST'])
+def trainFun():
     if checkUser(request):
         body = request.form.keys()[0]
 
@@ -129,7 +151,7 @@ def sendPictureFun():
         if not os.path.isdir(inputDirectory):
             os.makedirs(inputDirectory)
 
-        h5 = h5py.File(inputDirectory + 'data.h5', driver=None)
+        h5 = h5py.File(inputDirectory + 'data.h5', 'w', driver=None)
         if image.shape[3] == 1:
             # ilastik sends data in shape (x,y,z,1) when image is grayscale
             # we need (x,y,z)
@@ -140,7 +162,7 @@ def sendPictureFun():
                  image.shape[2]))
         h5.create_dataset('data', data=image)
         h5.close()
-        h5 = h5py.File(inputDirectory + 'labels.h5', driver=None)
+        h5 = h5py.File(inputDirectory + 'labels.h5', 'w', driver=None)
         if labels.shape[3] == 1:
             labels = np.reshape(
                 labels,
@@ -173,7 +195,10 @@ def sendPictureFun():
         logPath = os.getcwd() + "/userLogs/" + username
         if not os.path.isdir(logPath):
             os.makedirs(logPath)
-        logPath = logPath + "log.txt"
+        logPath = logPath + "/log.txt"
+	file = open(logPath, "w")
+	file.write("starting processing")
+	file.close()
         p = subprocess.Popen(
             ["python",
              "ccboost-service/handler.py",
@@ -183,9 +208,9 @@ def sendPictureFun():
              username], stdout=subprocess.PIPE, bufsize=1)
         for line in iter(p.stdout.readline, b''):
             print line,
-        # file = open(logPath, "w")
-        # file.write(line)
-        # file.close(),
+            #file = open(logPath, "a")
+            #file.write(line)
+            #file.close(),
         p.stdout.close()
         p.wait()
 
@@ -219,6 +244,113 @@ def sendPictureFun():
         # this shouldn't happen since the user can't send data before succesfully logging in
         return '', 401
 
+		
+@app.route('/api/testNewData', methods=['POST'])
+def testNewFun():
+    if checkUser(request):
+        body = request.form.keys()[0]
+
+        # for some reason the server doesn't receive + sings correctly
+        # which is why I translate them to ')' and back to '+'
+        # when sending data back and forth
+        body = body.translate(
+            string.maketrans(
+                custom_base64chars,
+                std_base64chars))
+
+        # removed during transfer but necessary for padding
+        body = body + '=='
+        bodyClear = base64.b64decode(body)
+
+        # get needed data from request
+        image = np.load(StringIO(bodyClear))['image']
+        username = request.headers['username']
+        datasetName = request.headers['datasetName']
+	modelName = request.headers['modelName']
+
+        # save data in h5 format
+        inputDirectory = os.getcwd() + '/userInput/' + username + "/" + datasetName + "/"
+        if not os.path.isdir(inputDirectory):
+            os.makedirs(inputDirectory)
+
+        h5 = h5py.File(inputDirectory + 'data.h5', 'w', driver=None)
+        if image.shape[3] == 1:
+            # ilastik sends data in shape (x,y,z,1) when image is grayscale
+            # we need (x,y,z)
+            image = np.reshape(
+                image,
+                (image.shape[0],
+                 image.shape[1],
+                 image.shape[2]))
+        h5.create_dataset('data', data=image)
+        h5.close()
+		
+        dir_path = os.getcwd() + "/ccboost-service/workspace"
+
+        # write cfg file based on request
+        if not os.path.isdir(dir_path + "/" + username + "/config"):
+            os.makedirs(dir_path + "/" + username + "/config")
+
+        file = open(dir_path + "/" + username + "/config/" + datasetName + ".cfg", "w")
+        file.write("root = \'/cvlabdata1/home/rizzello/ccboost-service\'\n")
+	file.write("dataset_name = \'" + datasetName + "\'\n")
+        file.write("stack = \'/cvlabdata1/home/rizzello/userInput/" + username + "/" + datasetName + "/data.h5\'\n")
+        file.write("model_name = " + modelName + "\n")
+        file.write("num_adaboost_stumps = 2000\n")
+        file.close()
+
+        logPath = os.getcwd() + "/userLogs/" + username
+        if not os.path.isdir(logPath):
+            os.makedirs(logPath)
+        logPath = logPath + "/log.txt"
+	file = open(logPath, "w")
+	file.write("starting processing")
+	file.close()
+        p = subprocess.Popen(
+            ["python",
+             "ccboost-service/handler.py",
+             "--test",
+             dir_path + "/" + username + "/config/" + datasetName + ".cfg",
+             "--username",
+             username], stdout=subprocess.PIPE, bufsize=1)
+        for line in iter(p.stdout.readline, b''):
+            print line,
+            #file = open(logPath, "a")
+            #file.write(line)
+            #file.close(),
+        p.stdout.close()
+        p.wait()
+
+        # fetch result and send it back
+        h5 = h5py.File(
+            dir_path +
+            "/" +
+            username +
+            "/runs/" +
+            datasetName +
+            "/results/" +
+            modelName +
+            "/out-0-ab-max.h5",
+            driver=None)
+        data = h5['data']
+        data = np.reshape(
+            data,
+            (data.shape[0],
+             data.shape[1],
+             data.shape[2],
+             1))
+
+        f = StringIO()
+        np.savez_compressed(f, image=data)
+        f.seek(0)
+        compressed_data = f.read()
+        # compressedData64 = base64.b64encode(compressed_data)
+
+        return compressed_data, 200
+    else:
+        # this shouldn't happen since the user can't send data before succesfully logging in
+        return '', 401
+		
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7171, ssl_context=context, threaded=True)
