d1b848dafee63ac39a4a2cddac80bc32f7b5de18
Eduard Trulls <etru1927@gmail.com>
2017-05-24 15:23:11 +0200

    Fix indentation.

--- server.py
+++ server.py
@@ -87,27 +87,29 @@ def loginFun():
     else:
         return '', 401
 
+
 @app.route('/api/downloadDataset', methods=['GET'])
 def downloadFun():
-	if checkUser(request):
-		dir_path = os.path.dirname(os.path.realpath(__file__))
-		username = request.headers['username']
-		datasetName = request.headers['dataset_name']
-		h5 = h5py.File(	dir_path +
-				"/userInput/"+
-				username+
-				"/"+
-				datasetName+
-				"/data.h5",
-				driver=None)
-        	data = h5['data']
-        	f = StringBytesIO()
-        	np.savez_compressed(f, data=data)
-        	f.seek(0)
-        	compressed_data = f.read()
-        	return compressed_data, 200
-	else:
-		return '', 401
+    if checkUser(request):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        username = request.headers['username']
+        datasetName = request.headers['dataset_name']
+        h5 = h5py.File(
+            dir_path +
+            "/userInput/" +
+            username +
+            "/" +
+            datasetName +
+            "/data.h5",
+            driver=None)
+        data = h5['data']
+        f = StringBytesIO()
+        np.savez_compressed(f, data=data)
+        f.seek(0)
+        compressed_data = f.read()
+        return compressed_data, 200
+    else:
+        return '', 401
 
 
 @app.route('/api/getProgress', methods=['GET'])
@@ -199,9 +201,9 @@ def trainFun():
         if not os.path.isdir(logPath):
             os.makedirs(logPath)
         logPath = logPath + "/log.txt"
-	file = open(logPath, "w")
-	file.write("starting processing \n")
-	file.close()
+        file = open(logPath, "w")
+        file.write("starting processing \n")
+        file.close()
         p = subprocess.Popen(
             ["python",
              "ccboost-service/handler.py",
@@ -210,9 +212,9 @@ def trainFun():
              "--username",
              username], stdout=subprocess.PIPE, bufsize=1)
         for line in iter(p.stdout.readline, b''):
-            	file = open(logPath, "a")
-            	file.write(line)
-            	file.close(),
+            file = open(logPath, "a")
+            file.write(line)
+            file.close(),
         p.stdout.close()
         p.wait()
 
@@ -234,21 +236,21 @@ def trainFun():
              data.shape[1],
              data.shape[2],
              1))
-	h5.close()
+        h5.close()
 
         f = StringBytesIO()
         np.savez_compressed(f, result=data)
         f.seek(0)
         compressed_data = f.read()
         # compressedData64 = base64.b64encode(compressed_data)
-	
-	os.remove(logPath)
+
+        os.remove(logPath)
         return compressed_data, 200
     else:
         # this shouldn't happen since the user can't send data before succesfully logging in
         return '', 401
 
-		
+
 @app.route('/api/testNewData', methods=['POST'])
 def testNewFun():
     if checkUser(request):
@@ -270,7 +272,7 @@ def testNewFun():
         image = np.load(StringBytesIO(bodyClear))['image']
         username = request.headers['username']
         datasetName = request.headers['datasetName']
-	modelName = request.headers['modelName']
+        modelName = request.headers['modelName']
 
         # save data in h5 format
         inputDirectory = os.getcwd() + '/userInput/' + username + "/" + datasetName + "/"
@@ -288,7 +290,7 @@ def testNewFun():
                  image.shape[2]))
         h5.create_dataset('data', data=image)
         h5.close()
-		
+
         dir_path = os.getcwd() + "/ccboost-service/workspace"
 
         # write cfg file based on request
@@ -297,9 +299,9 @@ def testNewFun():
 
         file = open(dir_path + "/" + username + "/config/" + datasetName + ".cfg", "w")
         file.write("root = \'/cvlabdata1/home/rizzello/ccboost-service\'\n")
-	file.write("dataset_name = \'" + datasetName + "\'\n")
+        file.write("dataset_name = \'" + datasetName + "\'\n")
         file.write("stack = \'/cvlabdata1/home/rizzello/userInput/" + username + "/" + datasetName + "/data.h5\'\n")
-	file.write("model_name = " + modelName + "\n")
+        file.write("model_name = " + modelName + "\n")
         file.write("num_adaboost_stumps = 2000\n")
         file.close()
 
@@ -307,9 +309,9 @@ def testNewFun():
         if not os.path.isdir(logPath):
             os.makedirs(logPath)
         logPath = logPath + "/log.txt"
-	file = open(logPath, "w")
-	file.write("starting processing \n")
-	file.close()
+        file = open(logPath, "w")
+        file.write("starting processing \n")
+        file.close()
         p = subprocess.Popen(
             ["python",
              "ccboost-service/handler.py",
@@ -342,7 +344,7 @@ def testNewFun():
              data.shape[1],
              data.shape[2],
              1))
-	h5.close()
+        h5.close()
 
         f = StringBytesIO()
         np.savez_compressed(f, result=data)
@@ -350,12 +352,11 @@ def testNewFun():
         compressed_data = f.read()
         # compressedData64 = base64.b64encode(compressed_data)
 
-	os.remove(logPath)
+        os.remove(logPath)
         return compressed_data, 200
     else:
         # this shouldn't happen since the user can't send data before succesfully logging in
         return '', 401
-
 
 
 @app.route('/api/testOldData', methods=['GET'])
@@ -363,7 +364,7 @@ def testOldFun():
     if checkUser(request):
         username = request.headers['username']
         datasetName = request.headers['datasetName']
-	modelName = request.headers['modelName']
+        modelName = request.headers['modelName']
 
         dir_path = os.getcwd() + "/ccboost-service/workspace"
 
@@ -373,9 +374,9 @@ def testOldFun():
 
         file = open(dir_path + "/" + username + "/config/" + datasetName + ".cfg", "w")
         file.write("root = \'/cvlabdata1/home/rizzello/ccboost-service\'\n")
-	file.write("dataset_name = \'" + datasetName + "\'\n")
+        file.write("dataset_name = \'" + datasetName + "\'\n")
         file.write("stack = \'/cvlabdata1/home/rizzello/userInput/" + username + "/" + datasetName + "/data.h5\'\n")
-	file.write("model_name = " + modelName + "\n")
+        file.write("model_name = " + modelName + "\n")
         file.write("num_adaboost_stumps = 2000\n")
         file.close()
 
@@ -383,9 +384,9 @@ def testOldFun():
         if not os.path.isdir(logPath):
             os.makedirs(logPath)
         logPath = logPath + "/log.txt"
-	file = open(logPath, "w")
-	file.write("starting processing \n")
-	file.close()
+        file = open(logPath, "w")
+        file.write("starting processing \n")
+        file.close()
         p = subprocess.Popen(
             ["python",
              "ccboost-service/handler.py",
@@ -418,17 +419,16 @@ def testOldFun():
              data.shape[1],
              data.shape[2],
              1))
-	h5.close()
+        h5.close()
 
-
-	h5 = h5py.File(
-		os.getcwd()+
-		"/userInput/"+
-		username+
-		"/"+
-		datasetName+
-		"/data.h5",
-            	driver=None)
+        h5 = h5py.File(
+            os.getcwd() +
+            "/userInput/" +
+            username +
+            "/" +
+            datasetName +
+            "/data.h5",
+            driver=None)
         image = h5['data']
         image = np.reshape(
             image,
@@ -436,8 +436,7 @@ def testOldFun():
              image.shape[1],
              image.shape[2],
              1))
-	h5.close()
-
+        h5.close()
 
         f = StringBytesIO()
         np.savez_compressed(f, image=image, result=data)
@@ -445,13 +444,12 @@ def testOldFun():
         compressed_data = f.read()
         # compressedData64 = base64.b64encode(compressed_data)
 
-
-	os.remove(logPath)
+        os.remove(logPath)
         return compressed_data, 200
     else:
         # this shouldn't happen since the user can't send data before succesfully logging in
         return '', 401
-		
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7171, ssl_context=context, threaded=True)
